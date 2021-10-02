b9c7aa4312ca43b7dfe45db2c403dc0b4740e52b
corvinus-tgpztt <mate.szladek@stud.uni-corvinus.hu>
2021-10-02 18:04:58 +0200

    kész lista és kapcsolatok

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
         }
+
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+
+
+      
+
+
+
     }
 }
