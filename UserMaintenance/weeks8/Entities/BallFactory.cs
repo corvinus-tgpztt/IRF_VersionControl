29db04818216b95e6b3b61dd3009d6518eaa4948
corvinus-tgpztt <mate.szladek@stud.uni-corvinus.hu>
2021-11-10 23:46:05 +0100

    A labdák készen állnak

---
+++ UserMaintenance/weeks8/Entities/BallFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weeks8.Entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+
+
+}
